54d94c9a010632c65625928a230eb795e39e3dcc
Thiago Castro <41759383+thytetgc@users.noreply.github.com>
2021-07-30 16:17:26 -0300

    Update nench.sh

--- nench/nench.sh
+++ nench/nench.sh
@@ -135,7 +135,7 @@ printf '\n'
 
 if ! command_exists ioping
 then
-    curl -s --max-time 10 -o ioping.static http://wget.racing/ioping.static
+    curl -s --max-time 10 -o ioping.static https://raw.githubusercontent.com/thytetgc/benchmark/main/nench/ioping.static
     chmod +x ioping.static
     ioping_cmd="./ioping.static"
 else
